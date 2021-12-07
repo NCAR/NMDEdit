384d20ef94aa6c7ea108d438fd019e2931c3beb2
Don Stott <stott@ucar.edu>
2021-12-07 11:16:51 -0700

    add how-to and update to UCP

--- Completeness/run_transforms-graphit.sh
+++ Completeness/run_transforms-graphit.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 
-foreach i (acom cgd cisl eol hao library mmm opensky ral rda unidata)
+foreach i (acom cgd cisl eol hao library mmm opensky ral rda ucp allInOne)
     java net.sf.saxon.Transform -s:WAFs/labs.xml -xsl:LevelsScores/ncarDialectScoresTXT.xsl -o:../xsl/out/Lab_scores/LevelsScores/$i-Scores.csv WAF_BASE=../WAFs recordSetPath=$i --suppressXsltNamespaceCheck:on
     echo "processed $i"
 end
